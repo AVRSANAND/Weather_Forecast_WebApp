3189901fb1193fe89b7feeb84ae11c56e448d049
AVRSANAND <avrsanand2003@gmail.com>
2024-06-20 19:42:40 +0530

    Get Filtered data results of Temperature and Sky

--- backend.py
+++ backend.py
@@ -1,11 +1,19 @@
 import requests
-API_KEY = "728a0bf7d6bcfea87796ce58cc2aae63"
+API_KEY = ""
 
 def get_data(place, days=None, data_option=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    values = 8 * days
+    filtered_data = filtered_data[:values]
+    if data_option == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if data_option == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
 
 if __name__ == "__main__":
-    print(get_data(place="Hyderabad"))
+    print(get_data(place="Hyderabad", days=3, data_option="Temperature"))
